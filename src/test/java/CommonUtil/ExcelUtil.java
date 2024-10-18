dc34a176f08fd7f9f311eb75bf8cc05c2a9c14e8
Ameet9087 <ameetsingh2000@gmail.com>
2024-10-18 18:45:13 +0530

    new candidate added ,status type as intrested

--- src/test/java/CommonUtil/ExcelUtil.java
+++ src/test/java/CommonUtil/ExcelUtil.java
@@ -2,28 +2,65 @@ package CommonUtil;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.poi.EncryptedDocumentException;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtil {
+		///////////////////////////////////////////////////////////////////////////////
 
-	public String getDataFromExcel(String SheetName,int RowNum,int ColNum) throws EncryptedDocumentException, IOException {
-		FileInputStream file=new FileInputStream("src\\test\\resources\\Excel.xlsx");
-		Workbook wbf=WorkbookFactory.create(file);
-		Sheet sheet = wbf.getSheet(SheetName);
-		Row row = sheet.getRow(RowNum);
-		Cell column = row.getCell(ColNum);
-		String value=column.getStringCellValue();
-		String strNumber = value + "";
+		    
+		    // Method to get cell value as string from an Excel sheet
+		    public static String getDataFromExcel(String excelFilePath, String sheetName, int rowNum, int colNum) {
+		        String cellData = "";
 
-		return strNumber;
-		
+		        try (FileInputStream fis = new FileInputStream(excelFilePath);
+		             Workbook workbook = new XSSFWorkbook(fis)) {
+
+		            Sheet sheet = workbook.getSheet(sheetName);
+		            if (sheet == null) {
+		                throw new IllegalArgumentException("Sheet " + sheetName + " does not exist.");
+		            }
+
+		            Row row = sheet.getRow(rowNum);
+		            if (row == null) {
+		                throw new IllegalArgumentException("Row " + rowNum + " does not exist.");
+		            }
+
+		            Cell cell = row.getCell(colNum);
+		            if (cell != null) {
+		                switch (cell.getCellType()) {
+		                    case STRING:
+		                        cellData = cell.getStringCellValue();
+		                        break;
+		                    case NUMERIC:
+		                        cellData = String.valueOf(cell.getNumericCellValue());
+		                        break;
+		                    case BOOLEAN:
+		                        cellData = String.valueOf(cell.getBooleanCellValue());
+		                        break;
+		                    case FORMULA:
+		                        cellData = cell.getCellFormula();
+		                        break;
+		                    default:
+		                        cellData = "";
+		                        break;
+		                }
+		            } else {
+		                throw new IllegalArgumentException("Cell at row " + rowNum + " and column " + colNum + " does not exist.");
+		            }
+
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+
+		        return cellData;
+		    }
+		////////////////////////////////////////////////////////////////////////////////
 	}
-}
+
--- src/test/java/recruiter/AddCandidateTestNG.java
+++ src/test/java/recruiter/AddCandidateTestNG.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -26,7 +27,7 @@ public class AddCandidateTestNG extends baseClass{
 
 	PropertyFileUtil pfu = new PropertyFileUtil();
 	WebDriverUtil wdu=new WebDriverUtil();
-	ExcelUtil eu = new ExcelUtil();
+	ExcelUtil eu=new ExcelUtil();
 	JavaUtil ju = new JavaUtil();
 	public WebDriver sdriver;
 	
@@ -41,37 +42,183 @@ public class AddCandidateTestNG extends baseClass{
 		r.RecruiterPage(driver);
 
 		Thread.sleep(2000);
-
+		
+		//login
 		loginPage lp = new loginPage(driver);
 		lp.login(USERNAME, PASSWORD);
 
 		
-		
+		//click on add candidate
 		RecruiterhomePage hp=new RecruiterhomePage(driver);
 		hp.addCan(driver);
 		
 		//get data from excel file
-		String CANDIDATE_NAME =eu.getDataFromExcel("AddCandidate", 0, 1);
-		String CANDIDATE_EMAIL =eu.getDataFromExcel("AddCandidate", 1, 1);
-	
-
-	    AddCandidate ac=new AddCandidate(driver);
-	    ac.CandidateInfo(CANDIDATE_NAME,CANDIDATE_EMAIL);
+		String CANDIDATE_NAME =eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate", 0, 1);
+		String CANDIDATE_EMAIL =eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate", 1, 1);
+		 String STATUS=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate", 3, 1);
+		 String CONTACT= eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate",4 , 1);
+		 String SOURCE=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate", 2,1);
+		 String FEEDBACK=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 6, 1);
+		String JOB_ID= eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate", 7, 1);
+		String LOCATION=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 8, 1);
+		String OTHER_LOC = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 9, 1);
+		String EDUCATION=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 12, 1);
+		String OTHER_EDUCATION=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 13, 1);
+		String TOTAL_EXP_YEAR = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 10, 1);
+		String TOTAL_EXP_MONTH = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 11, 1);
+		String RELEVENT_EXP=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 14, 1);
+		String NOTICE_PERIOD=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 15, 1);
+		String COMMUNICATION = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 16, 1);
+		String CURRENT_CTC = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 17, 1);
+		String EXPECTED_CTC = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 18, 1);
+		String OFFER_LETTER = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 19, 1);
+		String STATUS_TYPE = eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx", "AddCandidate", 20, 1);
 		
-	    Thread.sleep(1000);
-	    WebElement sourceName = driver.findElement(By.name("sourceName"));
-	    String SOURCE=eu.getDataFromExcel("AddCandidate", 2,1);
-	    Thread.sleep(1000);
-	    wdu.handleDropdown(sourceName, SOURCE);
+//		//enter candidateName and candidateEmail
+//	    AddCandidate ac=new AddCandidate(driver);
+//	    ac.CandidateInfo(CANDIDATE_NAME,CANDIDATE_EMAIL);
+		
+//	    //Source name select DropDown
+//	    Thread.sleep(1000);
+//	    WebElement sourceName = driver.findElement(By.name("sourceName"));
+//	    String SOURCE=eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate", 2,1);
+//	    Thread.sleep(1000);
+//	    wdu.handleDropdown(sourceName, SOURCE);
 	    
+	    //scrollDown to bottom to select status type
 	    JavascriptExecutor js = (JavascriptExecutor) driver;
 	    WebElement status = driver.findElement(By.name("selectYesOrNo"));
 	    js.executeScript("arguments[0].scrollIntoView();", status);
 	    
+//	    //enter candidate contact number
+//	   String CONTACT= eu.getDataFromExcel("src\\test\\resources\\Excel.xlsx","AddCandidate",4 , 1);
+//	    driver.findElement(By.cssSelector("input[name=\"contactNumber\"]")).sendKeys(CONTACT);
+	    
+	    //select status type from dropDown
 	    Thread.sleep(1000);
 	    WebElement statusType= driver.findElement(By.name("selectYesOrNo"));
-	    String STATUS=eu.getDataFromExcel("AddCandidate", 3, 1);
+	    statusType.click();
 	    Thread.sleep(1000);
 	    wdu.handleDropdown(statusType, STATUS);
+	    
+	    if (STATUS.equals("Interested")) {
+			System.out.println("Interested");
+			//enter candidateName and candidateEmail
+		    AddCandidate ac=new AddCandidate(driver);
+		    ac.CandidateInfo(CANDIDATE_NAME,CANDIDATE_EMAIL);
+		    
+		    //enter candidate contact number
+			driver.findElement(By.cssSelector("input[name=\"contactNumber\"]")).sendKeys(CONTACT);
+			
+			  //Source name select DropDown
+		    Thread.sleep(1000);
+		    WebElement sourceName = driver.findElement(By.name("sourceName"));
+		    sourceName.click();
+		    Thread.sleep(1000);
+		    wdu.handleDropdown(sourceName, SOURCE);
+		    
+		    //select feedback 
+		   WebElement feedback = driver.findElement(By.name("callingFeedback"));
+		   feedback.click();
+		   Thread.sleep(1000);
+		   wdu.handleDropdown(feedback, FEEDBACK);
+		  
+		   //select job id	    
+		   WebElement job_id = driver.findElement(By.id("requirementId"));
+		   job_id.click();
+		   Thread.sleep(1000);
+		   wdu.handleDropdown(job_id, JOB_ID);
+		   
+		   //select location
+		   WebElement location = driver.findElement(By.name("currentLocation"));
+		   location.click();
+		   Thread.sleep(1000);
+		   		if (LOCATION.equals("PCMC") || LOCATION.equals("Pune City")) {
+		   			wdu.handleDropdown(location, LOCATION);
+		   			System.out.println("pune");
+		   		} else if(LOCATION.equals("Other")) {
+		   			wdu.handleDropdown(location, LOCATION);
+		   			Thread.sleep(1000);
+		   			WebElement other = driver.findElement(By.cssSelector("input[name=\"currentLocation\"]"));
+		   			other.click();
+		   			other.clear();
+		   			other.sendKeys(OTHER_LOC);
+		   			System.out.println("equal to other");
+		   		}else if(!(LOCATION.equals("Other"))) {
+		   			
+		   			wdu.handleDropdown(location, OTHER_LOC);
+		   			Thread.sleep(1000);
+		   			WebElement other = driver.findElement(By.cssSelector("input[name=\"currentLocation\"]"));
+		   			other.click();
+		   			other.clear();
+		   			other.sendKeys(OTHER_LOC);
+		   			System.out.println("not equal");
+		   		}
+		   	
+		   //select education
+		   	WebElement education = driver.findElement(By.name("qualification"));
+		   	education.click();
+		   	Thread.sleep(1000);
+		   		if (EDUCATION.equals("Other")) {
+					wdu.handleDropdown(education, EDUCATION);
+					Thread.sleep(1000);
+					WebElement other = driver.findElement(By.cssSelector("input[name=\"qualification\"]"));
+					other.click();
+					other.sendKeys(OTHER_EDUCATION);
+				} else {
+					wdu.handleDropdown(education, EDUCATION);
+					System.out.println("select from dropdown");
+				}
+		   		
+		   		
+		   		//total experience
+		   		WebElement totalExpYear = driver.findElement(By.name("experienceYear"));
+		   		totalExpYear.sendKeys(TOTAL_EXP_YEAR);
+		   		WebElement totalExpMonth = driver.findElement(By.name("experienceMonth"));
+		   		totalExpMonth.sendKeys(TOTAL_EXP_MONTH);
+		   		
+		   		//relevent experience
+		   		WebElement releExp = driver.findElement(By.name("relevantExperience"));
+		   		releExp.sendKeys(RELEVENT_EXP);
+		   		//notice
+		   		WebElement notice = driver.findElement(By.name("noticePeriod"));
+		   		notice.sendKeys(NOTICE_PERIOD);
+		   		
+		   		//communication rating
+		   		WebElement communication = driver.findElement(By.name("communicationRating"));
+		   		communication.sendKeys(COMMUNICATION);
+		   		
+		   		//current CTC
+		   		WebElement current_ctc = driver.findElement(By.name("currentCTCLakh"));
+		   		current_ctc.sendKeys(CURRENT_CTC);
+		   		
+		   		//expected CTC
+		   		WebElement expected_ctc = driver.findElement(By.name("expectedCTCLakh"));
+		   		expected_ctc.sendKeys(EXPECTED_CTC);
+		   		
+		   		//scroll
+		   		js.executeScript("arguments[0].scrollIntoView();", status);
+		   		
+		   		//offer letter
+		   		WebElement offer_letter = driver.findElement(By.name("holdingAnyOffer"));
+		   		offer_letter.click();
+		   		Thread.sleep(1000);
+		   		wdu.handleDropdown(offer_letter, OFFER_LETTER);
+		   		
+		   		//status type
+		   		WebElement status_type = driver.findElement(By.name("finalStatus"));
+		   		offer_letter.click();
+		   		Thread.sleep(1000);
+		   		wdu.handleDropdown(status_type, STATUS_TYPE);
+		   		
+		   		Thread.sleep(1000);
+		   		driver.findElement(By.id("uploadbtn2")).click();
+		   		
+		   		Thread.sleep(1000);
+		   		driver.findElement(By.xpath("//button[text()=\"Yes\"]")).click();
+		   		
+		} else {
+			System.out.println("yet to confirm");
+		}
 	}
 }
